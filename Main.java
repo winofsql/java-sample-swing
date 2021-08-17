278503d2e96542c5cefe71c7e06db9cf9f5fe044
sworc <lightbox@nifty.com>
2021-08-18 00:31:36 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,18 +3,18 @@ import action.*;
 
 public class Main extends AppWindow {
 
-	// *****************************************************
-	// エントリポイント
-	// *****************************************************
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				Main thisClass = new Main();
-				thisClass.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				thisClass.setVisible(true);
-			}
-		});
-	}
+    // *****************************************************
+    // エントリポイント
+    // *****************************************************
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Main thisClass = new Main();
+                thisClass.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                thisClass.setVisible(true);
+            }
+        });
+    }
 
 }
